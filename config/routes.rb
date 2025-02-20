ded51a238d25032c607f34ef7fcdad590d8b938f
yuda12c <ngsrn.910@gmail.com>
2025-02-20 17:47:37 +0900

    商品詳細ページの情報表記の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :items, only: [:index, :new, :create, :show]
   root to: "items#index"
-  resources :items, only: [:index, :show, :new, :create]
 end
