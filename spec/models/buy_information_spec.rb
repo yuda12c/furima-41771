820dc793a5e11df2da598363f34f2a4524c1acce
yuda12c <ngsrn.910@gmail.com>
2025-03-17 21:32:28 +0900

    コードレビュー指摘箇所修正
    異常系テストコードのメッセージ追記

--- spec/models/buy_information_spec.rb
+++ spec/models/buy_information_spec.rb
@@ -35,7 +35,7 @@ describe '購入情報の保存' do
     it '郵便番号が空では無効' do
       @buy_information.postal_code = nil
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Postal code can't be blank")
     end
 
     it '郵便番号が「3桁-4桁」の形式でない場合は無効' do
@@ -43,50 +43,50 @@ describe '購入情報の保存' do
       invalid_postal_codes.each do |invalid_code|
         @buy_information.postal_code = invalid_code
         @buy_information.valid?
-        expect(@buy_information).not_to be_valid
+        expect(@buy_information.errors.full_messages).to include("Postal code is invalid. Enter as 123-4567")
       end
     end
 
     it '都道府県が空では無効' do
       @buy_information.place_id = 1
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Place can't be blank")
     end
 
     it '市区町村が空では無効' do
       @buy_information.city = nil
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("City can't be blank")
     end
 
     it '番地が空では無効' do
       @buy_information.address = nil
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Address can't be blank")
     end
 
     it '電話番号が空では無効' do
       @buy_information.phone_number = nil
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Phone number can't be blank")
     end
 
     it '電話番号が10桁未満では無効' do
       @buy_information.phone_number = '090123456'
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Phone number is invalid. Enter only numbers (10-11 digits)")
     end
 
     it '電話番号が11桁を超えると無効' do
       @buy_information.phone_number = '090123456789'
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Phone number is invalid. Enter only numbers (10-11 digits)")
     end
 
     it '電話番号にハイフンが含まれると無効' do
       @buy_information.phone_number = '090-1234-5678'
       @buy_information.valid?
-      expect(@buy_information).not_to be_valid
+      expect(@buy_information.errors.full_messages).to include("Phone number is invalid. Enter only numbers (10-11 digits)")
     end
 
     it '電話番号が半角数字以外を含むと無効' do
@@ -94,7 +94,7 @@ describe '購入情報の保存' do
       invalid_phone_numbers.each do |invalid_number|
         @buy_information.phone_number = invalid_number
         @buy_information.valid?
-        expect(@buy_information).not_to be_valid
+        expect(@buy_information.errors.full_messages).to include("Phone number is invalid. Enter only numbers (10-11 digits)")
       end
     end
 
